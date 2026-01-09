8938581966d4502ff0a15a4224dcbe49e24695b5
@itzzfalcon52 <hussaintaherkagalwala5253@gmail.com>
2026-01-09 16:29:14 +0530

    added user onboarding logic(i.e connecting clerk data with our prisma db)

---
+++ src/modules/auth/actions/index.js
@@ -0,0 +1,43 @@
+"use server"
+import db from "@/lib/db";
+import { currentUser } from "@clerk/nextjs/server";
+
+
+
+export async function onBoardUser(){
+    try{
+
+        const user=await currentUser();
+        if(!user){
+            return {success:false,message:"User not found"}
+        }
+       const {id,firstName,lastName,emailAddresses,imageUrl}=user;
+
+       const newUser=await db.user.upsert({  //upsert means if user doesnt exist then create that user,if he exists,then update it
+        where:{clerkId:id},
+        update:{
+            firstName:firstName||null,
+            lastName:lastName||null,
+            email:emailAddresses[0]?.emailAddress||"",
+            imageUrl: typeof imageUrl === "string" ? imageUrl : null
+        },
+        create:{
+            clerkId:id,
+            firstName:firstName||null,
+            lastName:lastName||null,
+            email:emailAddresses[0]?.emailAddress||null,
+            imageUrl: typeof imageUrl === "string" ? imageUrl : null
+        }
+       })
+
+       return({
+        success:true,
+        user:newUser,
+        message:"User onboarded successfully"
+       })
+    }catch(err){
+        console.error("Error onboarding user:",err);
+        return({success:false,message:"Internal server error"})
+
+    }
+}
